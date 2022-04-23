d3d0935a510cbb561e08d974fe2093fbe00ca8f0
piopoi <kangsw1988@gmail.com>
2022-04-23 22:00:55 +0900

    feat: CarPosition에 equals, hashCode 메서드 추가

--- src/main/java/racingcar/domain/CarPosition.java
+++ src/main/java/racingcar/domain/CarPosition.java
@@ -17,4 +17,23 @@ public class CarPosition {
         }
         this.position = position;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        CarPosition that = (CarPosition) o;
+
+        return position == that.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return position;
+    }
 }
