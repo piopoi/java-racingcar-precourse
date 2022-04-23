e88135e7ecad95f2fbc19e4f4536e8edc9a3d7cc
piopoi <kangsw1988@gmail.com>
2022-04-23 19:30:45 +0900

    refactor: 자동차 이름, 위치 변수에 대해 재할당 금지

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,8 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Car {
     private static final int STOP_MAX_NUMBER = 3; //3 이하일 경우 정지.
 
-    private CarName carName;
-    private CarPosition carPosition;
+    private final CarName carName;
+    private final CarPosition carPosition;
 
     public Car(String carName) {
         this.carName = new CarName(carName);
