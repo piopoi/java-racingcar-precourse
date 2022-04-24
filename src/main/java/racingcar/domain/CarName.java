a5346fe3a1d07892fbd2dfd32e7279b5a3bedccd
piopoi <kangsw1988@gmail.com>
2022-04-24 16:21:27 +0900

    refactor: CarName 변경
    CarName
    - 비교를 위한 equals, hashCode Override
    - 자동차 이름 길이 체크를 별도 메서드로 분리
    - 상수 추가

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,21 +1,41 @@
 package racingcar.domain;
 
 public class CarName {
-    private static final int MIN_NAME_LENGTH = 5;
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final int MIN_NAME_LENGTH = 1;
     private final String name;
 
     public CarName(String name) {
-        if (!nameLengthCheck(name)) {
-            throw new IllegalArgumentException("[ERROR] 자동차이름은 5자 이하로 입력해주세요.");
-        }
+        checkNameLength(name);
         this.name = name;
     }
 
-    private static boolean nameLengthCheck(String name) {
-        return name.length() <= MIN_NAME_LENGTH;
+    private static void checkNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH || name.length() < MIN_NAME_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 자동차이름은 1~5자로 입력해주세요.");
+        }
     }
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        CarName carName = (CarName) o;
+
+        return name != null ? name.equals(carName.name) : carName.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
     }
 }
