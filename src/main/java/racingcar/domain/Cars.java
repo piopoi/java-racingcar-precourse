3441127d41b2676a33a31cb4858c18606fc19f66
piopoi <kangsw1988@gmail.com>
2022-04-24 13:45:11 +0900

    feat: Cars 클래스에 레이싱 결과 반환 기능 추가
    Cars
    - 자동차 세팅 시 최소 2대 이상이도록 체크하는 로직 추가
    - 승리자 위치 조회 기능 추가
    - 승리자 여부 확인 기능 추가
    - 승리한 Car List 반환 기능 추가
    - 게임결과(RacingResult) 반환 기능 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,12 +1,19 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import racingcar.util.RandomUtils;
 
 public class Cars {
+    public static final int MIN_CAR_COUNT = 2; //최소 차량 대수
+
     private final List<Car> cars;
 
     public Cars(List<Car> cars) {
+        if (cars.size() < MIN_CAR_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 최소 2대 이상의 자동차 이름을 입력해주세요.");
+        }
         this.cars = cars;
     }
 
@@ -21,5 +28,29 @@ public class Cars {
             String printString = car.getName() + " : " + car.getHyphenRoad();
             System.out.println(printString);
         }
+    }
+
+    public RacingResult getRacingResult() {
+        CarPosition WinnerPosition = getWinnerPosition();
+        List<Car> Winners = getWinners();
+        return new RacingResult(Winners);
+    }
+
+    private List<Car> getWinners() {
+        List<Car> Winners = new ArrayList<>(cars);
+        Winners.removeIf(car -> {
+            CarPosition WinnerPosition = getWinnerPosition();
+            return !isWinner(car, WinnerPosition);
+        });
+        return Winners;
+    }
+
+    private CarPosition getWinnerPosition() {
+        Car WinnerCar = Collections.max(cars);
+        return WinnerCar.getPosition();
+    }
+
+    boolean isWinner(Car car, CarPosition WinnerPosition) {
+        return WinnerPosition.equals(car.getPosition());
     }
 }
